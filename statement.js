5a9f1f89f2243263dcd97e18518a3fbb08341459
Ronald Escobar <ronaldescobarj@gmail.com>
2019-03-03 19:17:58 -0400

    reemplazada variable por playFor. tests pasando

--- statement.js
+++ statement.js
@@ -2,9 +2,9 @@ function playFor(aPerformance, plays) {
   return plays[aPerformance.playID];
 }
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance, plays) {
   let result = 0;
-  switch(play.type) {
+  switch (playFor(aPerformance, plays).type) {
     case "tragedy":
       result = 40000;
       if (aPerformance.audience > 30) {
@@ -19,7 +19,7 @@ function amountFor(aPerformance, play) {
       result += 300 * aPerformance.audience;
       break;
     default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance, plays).type}`);
   }
   return result;
 }
@@ -32,7 +32,7 @@ function statement (invoice, plays) {
                       { style: "currency", currency: "USD",
                         minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf, plays));
+    let thisAmount = amountFor(perf, plays);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
