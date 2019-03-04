fbb8550175b8eeb7f47c4dfe3a6c2ce228112d2a
Ronald Escobar <ronaldescobarj@gmail.com>
2019-03-03 21:47:29 -0400

    reordenado codigo. tests pasando

--- statement.js
+++ statement.js
@@ -3,15 +3,20 @@ var plays = JSON.parse(fs.readFileSync('./plays.json', 'utf8'));
 var inv_json = JSON.parse(fs.readFileSync('./invoices.json', 'utf8'));
 var invoice = inv_json[0];
 
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
+function statement (invoice, plays) {
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
-function totalVolumeCredits() {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let perf of data.performances) {
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
   }
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
 
@@ -23,12 +28,32 @@ function totalAmount() {
   return result;
 }
 
+function totalVolumeCredits() {
+  let result = 0;
+  for (let perf of invoice.performances) {
+    result += volumeCreditsFor(perf);
+  }
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
     {
       style: "currency", currency: "USD",
       minimumFractionDigits: 2
-    }).format(aNumber/100);
+    }).format(aNumber / 100);
+}
+
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
+  result += Math.max(aPerformance.audience - 30, 0);
+  if ("comedy" === playFor(aPerformance).type) result +=
+    Math.floor(aPerformance.audience / 5);
+  return result;
+}
+
+function playFor(aPerformance) {
+  return plays[aPerformance.playID];
 }
 
 function amountFor(aPerformance) {
@@ -53,29 +78,4 @@ function amountFor(aPerformance) {
   return result;
 }
 
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-  if ("comedy" === playFor(aPerformance).type) result +=
-    Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function statement (invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
-  return renderPlainText(statementData, invoice, plays);
-}
-
-function renderPlainText(data, plays) {
-  let result = `Statement for ${data.customer}\n`;
-  for (let perf of data.performances) {
-    // print line for this order
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-  }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
-  return result;
-}
 export default statement;
