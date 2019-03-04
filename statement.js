6dc8c624624fbfe43b9ba4ecf67a79595c7bfbcc
Ronald Escobar <ronaldescobarj@gmail.com>
2019-03-03 21:02:01 -0400

    agregada funcion render plain text. tests pasando

--- statement.js
+++ statement.js
@@ -62,6 +62,10 @@ function volumeCreditsFor(aPerformance) {
 }
 
 function statement (invoice, plays) {
+  return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     // print line for this order
@@ -71,5 +75,4 @@ function statement (invoice, plays) {
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 }
-
 export default statement;
